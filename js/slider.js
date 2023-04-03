8aa464a76f62cab690538cc38b6d07ac2ab3fdb4
Yesica Perlaza <perlazayesik@gmail.com>
2023-04-03 18:47:30 -0500

    terminando la seccion de eventos

--- js/slider.js
+++ js/slider.js
@@ -3,15 +3,27 @@
 
     const d = document;
     const w = window;
-    const slider = [...d.querySelectorAll(".hero__content--imagenes")];
+    const slider = [...d.querySelectorAll(".event__cont")];
+    let valor;
 
-    w.addEventListener("DOMContentLoaded",(el)=>{
-    
-            setInterval(slider.forEach(e => e.classList.toggle("hero__content--imagenesShow")),1000);
-        
-    })
+    const mover = (cambio)=>{
+        const actual = Number(d.querySelector(".event__cont--show").dataset.id);
+
+        valor = actual;
+        valor += cambio;
+
+        if(valor === 0 || valor === slider.length + 1){
+            valor = valor === 0 ? slider.length : 1;
+        }
+
+        slider[actual-1].classList.toggle("event__cont--show");
+        slider[valor-1].classList.toggle("event__cont--show");
+    }
      
-
+    d.addEventListener("click",(e)=>{
+        if(e.target.matches(".btn__izq")) mover(-1);
+        if(e.target.matches(".btn__der")) mover(1);
+    })
 
 
 })();
