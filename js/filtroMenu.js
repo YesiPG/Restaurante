4a735a4ef2a1f2695bb3e2523f31c2a759cbed33
Yesica Perlaza <perlazayesik@gmail.com>
2023-04-10 19:26:20 -0500

    empezando con filtroMenu.js

--- js/filtroMenu.js
+++ js/filtroMenu.js
@@ -1,5 +1,7 @@
 (function(){
     "use strict";
     const d = document;
-    
+
+    const contenedor = [...d.querySelectorAll(".menu__cont--platos-btn")];
+    const boton = [...d.querySelectorAll(".menu__botones--btn")];
 })()
